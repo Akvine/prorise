9c486c7a00c05947a4c7de12dfb71fcc55bf7db7
star4okk <jasmas@ro.ru>
2023-02-20 00:31:41 +0300

    init project

---
+++ src/test/java/ru/akvine/prorise/ProriseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.prorise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
